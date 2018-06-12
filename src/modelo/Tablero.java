ccbf674b5855044526a8293cd82f31abcef04201
victor <victor@vmint>
2018-06-12 20:55:51 -0300

    coleccciones a tablero

--- src/modelo/Tablero.java
+++ src/modelo/Tablero.java
@@ -1,7 +1,33 @@
 package modelo;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Tablero {
 
+	private Collection<CartaMonstruo> zonaDeMonstruos;
+	private Collection<Carta> zonaMagica;
+	private Collection<Carta> cementerio;
+	private CartaDeCampo cartaDeCampo;
+	
+	public Tablero() {
+		this.zonaDeMonstruos = new ArrayList<CartaMonstruo>();
+		this.zonaMagica = new ArrayList<Carta>();
+		this.cementerio = new ArrayList<Carta>();
+	}
+	
+	public void agregar(CartaMonstruo unaCartaMonstruo) {
+		this.zonaDeMonstruos.add(unaCartaMonstruo);	
+	}
+	
+	public void agregar(CartaMagica unaCartaMagica) {
+		this.zonaMagica.add(unaCartaMagica);
+	}
+	
+	public void agregar(CartaTrampa unaCartaDeTrampa) {
+		this.zonaMagica.add(unaCartaDeTrampa);
+	}
+	
 	public void agregarBocaAbajo(Carta carta) {
 		carta.asignarPosicion(new BocaAbajo());
 	}
