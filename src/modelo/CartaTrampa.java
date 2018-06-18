0f2dcef9318ff187cc3fe88c5cbe9875ab9927c7
SergioCalvani <sergiocalvani@outlook.com>
2018-06-18 16:51:26 -0300

    Se agregaron métodos necesarios para poder activar cartas trampa

--- src/modelo/CartaTrampa.java
+++ src/modelo/CartaTrampa.java
@@ -1,9 +1,20 @@
 package modelo;
 
 public class CartaTrampa extends Carta {
+	
+	protected Jugador duenio;
 
 	public CartaTrampa(String nombre) {
 		super(nombre);
 	}
+	
+	public String obtenerNombre(){
+		return this.nombre;
+	}
+	
+	public void asignarDuenio(Jugador nuevoDuenio){
+		this.duenio = nuevoDuenio;
+	}
 
+	public void activar(CartaMonstruo cartaAtacada, CartaMonstruo cartaParaAtacar) {};
 }
