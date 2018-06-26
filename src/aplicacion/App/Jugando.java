8b8dafea374d9eece4ae09cbb8b057e7717f994e
SergioCalvani <sergiocalvani@outlook.com>
2018-06-25 22:45:17 -0300

    Prueba funcionando

--- src/aplicacion/App/Jugando.java
+++ src/aplicacion/App/Jugando.java
@@ -1,15 +1,25 @@
 package aplicacion.App;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
-import vista.VistaCarta;
-import vista.VistaMano;
-import vista.VistaTablero;
+import modelo.CartaMagica;
 import modelo.CartaMonstruo;
 import modelo.Jugador;
 import modelo.Mano;
 import modelo.Tablero;
+import modelo.cartas.AgujeroOscuro;
+import modelo.cartas.Fisura;
+import modelo.cartas.Jinzo;
+import vista.VistaAgujeroOscuro;
+import vista.VistaFisura;
+import vista.VistaJinzo;
+import vista.VistaMagica;
+import vista.VistaMano;
+import vista.VistaMonstruo;
+import vista.VistaTablero;
 
 public class Jugando extends Application{
 	
@@ -25,9 +35,21 @@ public class Jugando extends Application{
 		Mano unaMano = new Mano();
 		VistaMano mano = new VistaMano(unaMano);
 		VistaTablero tablero = new VistaTablero(tab,jugadorUno,jugadorDos,mano);
-		CartaMonstruo carta = new CartaMonstruo("Ejemplo",200,300,4);
-		VistaCarta figuraCarta = new VistaCarta(carta,tablero);
+		CartaMagica carta = new AgujeroOscuro();
+		VistaMagica figuraCarta = new VistaAgujeroOscuro (carta,tablero);
+		CartaMonstruo carta2 = new CartaMonstruo("Ejemplo2",200,300,4);
+		VistaMonstruo  figuraCarta2 = new VistaMonstruo (carta2,tablero);
+		CartaMonstruo carta3 = new Jinzo();
+		VistaMonstruo figuraCarta3 = new VistaJinzo(carta3,tablero);
+		CartaMagica carta4 = new Fisura();
+		VistaMagica figuraCarta4 = new VistaFisura(carta4,tablero);
+		CartaMonstruo carta5 = new CartaMonstruo("Ejemplo5",200,300,4);
+		VistaMonstruo  figuraCarta5 = new VistaMonstruo (carta5,tablero);
 		mano.agregarCarta(figuraCarta);
+		mano.agregarCarta(figuraCarta2);
+		mano.agregarCarta(figuraCarta3);
+		mano.agregarCarta(figuraCarta4);
+		mano.agregarCarta(figuraCarta5);
 		tablero.cargarMano();
 		Scene escena = new Scene(tablero.obtenerFigura(),1280,700);
 		primaryStage.setScene(escena);
