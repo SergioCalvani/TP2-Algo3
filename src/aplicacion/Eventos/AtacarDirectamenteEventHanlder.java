46fa35eef83ee3958ffe5f4d08a1aed8b22f24ea
lucas <lucas_rack@live.com.ar>
2018-06-28 19:15:32 -0300

    Informe

--- src/aplicacion/Eventos/AtacarDirectamenteEventHanlder.java
+++ src/aplicacion/Eventos/AtacarDirectamenteEventHanlder.java
@@ -36,7 +36,7 @@ public class AtacarDirectamenteEventHanlder implements EventHandler<ActionEvent>
 			this.ventana.close();
 		}
 		catch(NoSePuedeAtacarDirectamenteException e){
-			File song = new File( "src/vista/sonidos/error.mp3");
+			File song = new File( "src/aplicacion/Sonidos/error.mp3");
 			Media media = new Media(song.toURI().toString());
 		    MediaPlayer player = new MediaPlayer(media);
 		    player.setAutoPlay(true);
